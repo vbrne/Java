f708b773d0a1899f00b712dfcba6b9f6809c263e
brne <villalon.bernie@gmail.com>
2020-11-02 18:05:47 -0600

    scrapped comms idea

--- sourceCode/NMOSComs.java
+++ sourceCode/NMOSComs.java
@@ -7,147 +7,94 @@
  * Group 15: Bernie VIllalon, Samuel Solis, Leo Marroquin
  *
  * Description:
- *   This Class is responsible for communicating with the USB-Module. 
+ *   This Class is responsible for communicating with C++ program that reads
+ *  and writes to the USB-Module. 
  *
  ******************************************************************************/
 
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import java.util.Enumeration;
-import java.util.HashSet;
-
-import gnu.io.*;  // RXTXComm.jar Library
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.Scanner;
 
 public class NMOSComs {
+  File flg = new File("h7f5k68k.DAT");
+  File vds = new File("bk851d39.DAT");
+  File vgs = new File("y6u4w0r7.DAT");
+  File vth = new File("eltonj.DAT");
+  File drp = new File("drp.DAT");
 
-  void connect(String portName) throws Exception {
-    CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
-
-    if (portIdentifier.isCurrentlyOwned()) System.out.println("Error: Port is currently in use");
-    else {
-      int timeout = 2000;
-      CommPort commPort = portIdentifier.open(this.getClass().getName(), timeout);
-
-      if(commPort instanceof SerialPort) {
-        SerialPort serialPort = (SerialPort)commPort;
-        serialPort.setSerialPortParams(57600,
-                                      SerialPort.DATABITS_8,
-                                      SerialPort.STOPBITS_1,
-                                      SerialPort.PARITY_NONE);
-
-        InputStream in = serialPort.getInputStream();
-        OutputStream out = serialPort.getOutputStream();
-
-        (new Thread(new SerialReader(in))).start();
-        (new Thread(new SerialWriter(out))).start();
-
-      } 
-      else System.out.println("Error: Only serial ports are handled by this example.");
-    }
+  public NMOSComs() {
+    setFile(flg);
+    setFile(vds);
+    setFile(vgs);
+    setFile(vth);
+    setFile(drp);
   }
 
-  public static class SerialReader implements Runnable {
-    InputStream in;
-    public SerialReader(InputStream in) { this.in = in; }
-
-    public void run() {
-      byte[] buffer = new byte[1024];
-      int len = -1;
-      try {
-        while ((len = this.in.read(buffer)) > -1) {
-          System.out.print(new String(buffer, 0, len));
-        }
-      } 
-      catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
-  }
-
-  public static class SerialWriter implements Runnable {
-
-    OutputStream out;
-
-    public SerialWriter(OutputStream out) {
-      this.out = out;
-    }
-
-    public void run() {
-      try {
-        int c = 0;
-        while ((c = System.in.read()) > -1) {
-          this.out.write( c );
-        }
-      } 
-      catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
-  }
-
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-  static void listPorts() {
-      Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
-      while (portEnum.hasMoreElements()) {
-          CommPortIdentifier portIdentifier = portEnum.nextElement();
-          System.out.println(portIdentifier.getName()  +  " - " +  getPortTypeName(portIdentifier.getPortType()));
-      }        
-  }
-  
-  static String getPortTypeName (int portType) {
-      switch (portType) {
-          case CommPortIdentifier.PORT_I2C:      return "I2C";
-          case CommPortIdentifier.PORT_PARALLEL: return "Parallel";
-          case CommPortIdentifier.PORT_RAW:      return "Raw";
-          case CommPortIdentifier.PORT_RS485:    return "RS485";
-          case CommPortIdentifier.PORT_SERIAL:   return "Serial";
-          default:                               return "unknown type";
-      }
-  }
-
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-  
-  // @return    A HashSet containing the CommPortIdentifier for all serial ports that are not currently being used.
-  
-  public static HashSet<CommPortIdentifier> getAvailableSerialPorts() {
-      HashSet<CommPortIdentifier> h = new HashSet<CommPortIdentifier>();
-      Enumeration thePorts = CommPortIdentifier.getPortIdentifiers();
-      while (thePorts.hasMoreElements()) {
-          CommPortIdentifier com = (CommPortIdentifier) thePorts.nextElement();
-          switch (com.getPortType()) {
-          case CommPortIdentifier.PORT_SERIAL:
-              try {
-                  CommPort thePort = com.open("CommUtil", 50);
-                  thePort.close();
-                  h.add(com);
-              } catch (PortInUseException e) {
-                  System.out.println("Port, "  + com.getName() + ", is in use.");
-              } catch (Exception e) {
-                  System.err.println("Failed to open port " +  com.getName());
-                  e.printStackTrace();
-              }
-          }
-      }
-      return h;
-  }
-
-  public static void main(String[] args) {
+  public void startSweep1() {
     try {
-      NMOSComs test = new NMOSComs();
-      //for (int i = 0; i < 8; i++);
-      test.connect("/dev/ttyS4");
-      //test.listPorts();
-      //test.getAvailableSerialPorts();
-    } 
-    catch (Exception e) {
+      FileWriter tmp = new FileWriter(flg.getName());
+      tmp.write("true");
+      tmp.close();
+
+      while(true) {
+        Scanner readVds = new Scanner(vds);         // Initiates Scanner
+        Scanner readVgs = new Scanner(vgs);         // Initiates Scanner
+        Scanner readDrp = new Scanner(drp);         // Initiates Scanner
+
+        String sigVds = readVds.nextLine();         // Stores first line
+        String sigVgs = readVgs.nextLine();         // Stores first line
+        String sigDrp = readDrp.nextLine();         // Stores first line
+
+        readVds.close();                            // Closes files
+        readVgs.close();                            // Closes files
+        readDrp.close();                            // Closes files
+
+        System.out.println(sigVds + ", " + sigVgs); // For Debug
+
+        if (sigVds == "true" && sigVgs == "true" && sigDrp == "true") break; // Check if files were updated
+
+        try {                                       // Delay
+          Thread.sleep(250);
+        } catch (InterruptedException ie) {
+          Thread.currentThread().interrupt();
+        }
+      }
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
       e.printStackTrace();
     }
   }
-}
 
+  private void setFile(File f) {
+    try {
+      if (f.createNewFile()) System.out.println("File created: " + f.getName());
+      FileWriter tmp = new FileWriter(f.getName());
+      tmp.write("false");
+      tmp.close();
+      System.out.println("Successfully set " + f.getName());
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+    NMOSComs n = new NMOSComs();
+    n.startSweep1();
+    /*try {
+      File myObj = new File("filename.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }*/
+  }
+}
